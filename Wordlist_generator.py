f7cf114a629764521f6967ec6fc66726d46f89fb
thewhiterabbithack <thewhiterabbit_hack@protonmail.com>
2025-09-02 20:28:53 +0200

    Wordlist v.1

---
+++ Wordlist_generator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+# Introducimos la palabra y el número a combinar
+
+parser = argparse.ArgumentParser(description="Generador de wordlist")
+parser.add_argument('-p', '--palabra', help="Palabra para combinar")
+parser.add_argument('-n', '--numero', type = int, help="Número para combinar")
+args = parser.parse_args()
+
+ 
+
+archivo_salida = 'wordlist.txt'
+try:            
+    with open(archivo_salida, 'w') as f:
+        for i in range (args.numero):
+            combinacion = args.palabra + str(i)
+            f.write(combinacion + '\n')
+
+    print (f'El wordlist se ha creado en: {archivo_salida}')
+except:
+   print ('No se ha podido generar el archivo')
+
+
+   
+
